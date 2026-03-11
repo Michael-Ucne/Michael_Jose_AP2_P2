a95615afe6b98abf217eac9c6cdf40774d0b89fa
Dev0x1 <byte0x1@gmail.com>
2026-03-11 19:05:43 -0400

    Agrega el ListViewModel para consultar la api.

---
+++ app/src/main/java/com/phantomshard/michael_jose_ap2_p2/presentation/list/JugadorListViewModel.kt
@@ -0,0 +1,51 @@
+package com.phantomshard.michael_jose_ap2_p2.presentation.list
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.phantomshard.michael_jose_ap2_p2.data.remote.Resource
+import com.phantomshard.michael_jose_ap2_p2.domain.repository.JugadorRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class JugadorListViewModel @Inject constructor(
+    private val repository: JugadorRepository
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(JugadorListUiState())
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        getJugadores()
+    }
+
+    fun getJugadores() {
+        viewModelScope.launch {
+            _uiState.update { it.copy(isLoading = true) }
+            val result = repository.getJugadores()
+            when (result) {
+                is Resource.Success -> {
+                    _uiState.update {
+                        it.copy(
+                            isLoading = false,
+                            jugadores = result.data ?: emptyList()
+                        )
+                    }
+                }
+                is Resource.Error -> {
+                    _uiState.update {
+                        it.copy(
+                            isLoading = false,
+                            error = result.message
+                        )
+                    }
+                }
+                is Resource.Loading -> { }
+            }
+        }
+    }
+}
